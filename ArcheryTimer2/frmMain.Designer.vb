383ae1874ea62655349a095da423516bd1c611d9
t-midorikawa <t-midorikawa@kyouikusangyou.co.jp>
2024-01-16 20:09:33 +0900

    wow!

---
+++ ArcheryTimer2/frmMain.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblTimer = New System.Windows.Forms.Label()
+        Me.btnSetting = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnPause = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblTimer
+        '
+        Me.lblTimer.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblTimer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTimer.Font = New System.Drawing.Font("Arial Narrow", 399.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimer.Location = New System.Drawing.Point(12, 9)
+        Me.lblTimer.Name = "lblTimer"
+        Me.lblTimer.Size = New System.Drawing.Size(968, 468)
+        Me.lblTimer.TabIndex = 0
+        Me.lblTimer.Text = "9:99"
+        Me.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnSetting
+        '
+        Me.btnSetting.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnSetting.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnSetting.Location = New System.Drawing.Point(12, 480)
+        Me.btnSetting.Name = "btnSetting"
+        Me.btnSetting.Size = New System.Drawing.Size(130, 54)
+        Me.btnSetting.TabIndex = 5
+        Me.btnSetting.Text = "設定"
+        Me.btnSetting.UseVisualStyleBackColor = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnStart.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnStart.Location = New System.Drawing.Point(442, 480)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(130, 54)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "開始"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnPause
+        '
+        Me.btnPause.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnPause.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnPause.Location = New System.Drawing.Point(578, 480)
+        Me.btnPause.Name = "btnPause"
+        Me.btnPause.Size = New System.Drawing.Size(130, 54)
+        Me.btnPause.TabIndex = 2
+        Me.btnPause.Text = "中断"
+        Me.btnPause.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnStop.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnStop.Location = New System.Drawing.Point(714, 480)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(130, 54)
+        Me.btnStop.TabIndex = 3
+        Me.btnStop.Text = "停止"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnQuit.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnQuit.Location = New System.Drawing.Point(850, 480)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(130, 54)
+        Me.btnQuit.TabIndex = 4
+        Me.btnQuit.Text = "終了"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(992, 546)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnPause)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnSetting)
+        Me.Controls.Add(Me.lblTimer)
+        Me.Name = "frmMain"
+        Me.Text = "ArcheryTimer2"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lblTimer As Label
+    Friend WithEvents btnSetting As Button
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnPause As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ ArcheryTimer2/frmSetting.Designer.vb
@@ -0,0 +1,298 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSetting
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gbTime = New System.Windows.Forms.GroupBox()
+        Me.gbColor = New System.Windows.Forms.GroupBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.lblTime0 = New System.Windows.Forms.Label()
+        Me.txtTime0 = New System.Windows.Forms.TextBox()
+        Me.lblTime3 = New System.Windows.Forms.Label()
+        Me.lblTime1 = New System.Windows.Forms.Label()
+        Me.lblTime4 = New System.Windows.Forms.Label()
+        Me.txtTime1 = New System.Windows.Forms.TextBox()
+        Me.lblTime2 = New System.Windows.Forms.Label()
+        Me.lblTime5 = New System.Windows.Forms.Label()
+        Me.txtTime2 = New System.Windows.Forms.TextBox()
+        Me.btnColor0 = New System.Windows.Forms.Button()
+        Me.lblColor0 = New System.Windows.Forms.Label()
+        Me.lblColor1 = New System.Windows.Forms.Label()
+        Me.btnColor1 = New System.Windows.Forms.Button()
+        Me.lblColor2 = New System.Windows.Forms.Label()
+        Me.btnColor2 = New System.Windows.Forms.Button()
+        Me.lblColor3 = New System.Windows.Forms.Label()
+        Me.btnColor3 = New System.Windows.Forms.Button()
+        Me.gbTime.SuspendLayout()
+        Me.gbColor.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gbTime
+        '
+        Me.gbTime.Controls.Add(Me.lblTime2)
+        Me.gbTime.Controls.Add(Me.lblTime5)
+        Me.gbTime.Controls.Add(Me.txtTime2)
+        Me.gbTime.Controls.Add(Me.lblTime1)
+        Me.gbTime.Controls.Add(Me.lblTime4)
+        Me.gbTime.Controls.Add(Me.txtTime1)
+        Me.gbTime.Controls.Add(Me.lblTime0)
+        Me.gbTime.Controls.Add(Me.lblTime3)
+        Me.gbTime.Controls.Add(Me.txtTime0)
+        Me.gbTime.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.gbTime.Location = New System.Drawing.Point(12, 8)
+        Me.gbTime.Name = "gbTime"
+        Me.gbTime.Size = New System.Drawing.Size(210, 135)
+        Me.gbTime.TabIndex = 0
+        Me.gbTime.TabStop = False
+        Me.gbTime.Text = "時間設定"
+        '
+        'gbColor
+        '
+        Me.gbColor.Controls.Add(Me.lblColor3)
+        Me.gbColor.Controls.Add(Me.btnColor3)
+        Me.gbColor.Controls.Add(Me.lblColor2)
+        Me.gbColor.Controls.Add(Me.btnColor2)
+        Me.gbColor.Controls.Add(Me.lblColor1)
+        Me.gbColor.Controls.Add(Me.btnColor1)
+        Me.gbColor.Controls.Add(Me.lblColor0)
+        Me.gbColor.Controls.Add(Me.btnColor0)
+        Me.gbColor.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.gbColor.Location = New System.Drawing.Point(12, 149)
+        Me.gbColor.Name = "gbColor"
+        Me.gbColor.Size = New System.Drawing.Size(210, 185)
+        Me.gbColor.TabIndex = 1
+        Me.gbColor.TabStop = False
+        Me.gbColor.Text = "表示色設定"
+        '
+        'btnOk
+        '
+        Me.btnOk.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnOk.Location = New System.Drawing.Point(145, 338)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(81, 33)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "登録"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(12, 338)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(81, 33)
+        Me.btnCancel.TabIndex = 3
+        Me.btnCancel.Text = "取消"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'lblTime0
+        '
+        Me.lblTime0.AutoSize = True
+        Me.lblTime0.Location = New System.Drawing.Point(25, 27)
+        Me.lblTime0.Name = "lblTime0"
+        Me.lblTime0.Size = New System.Drawing.Size(72, 16)
+        Me.lblTime0.TabIndex = 0
+        Me.lblTime0.Text = "事前秒数"
+        '
+        'txtTime0
+        '
+        Me.txtTime0.Location = New System.Drawing.Point(102, 24)
+        Me.txtTime0.Name = "txtTime0"
+        Me.txtTime0.Size = New System.Drawing.Size(66, 23)
+        Me.txtTime0.TabIndex = 1
+        Me.txtTime0.Text = "10"
+        Me.txtTime0.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblTime3
+        '
+        Me.lblTime3.AutoSize = True
+        Me.lblTime3.Location = New System.Drawing.Point(173, 27)
+        Me.lblTime3.Name = "lblTime3"
+        Me.lblTime3.Size = New System.Drawing.Size(24, 16)
+        Me.lblTime3.TabIndex = 2
+        Me.lblTime3.Text = "秒"
+        '
+        'lblTime1
+        '
+        Me.lblTime1.AutoSize = True
+        Me.lblTime1.Location = New System.Drawing.Point(25, 56)
+        Me.lblTime1.Name = "lblTime1"
+        Me.lblTime1.Size = New System.Drawing.Size(72, 16)
+        Me.lblTime1.TabIndex = 3
+        Me.lblTime1.Text = "設定秒数"
+        '
+        'lblTime4
+        '
+        Me.lblTime4.AutoSize = True
+        Me.lblTime4.Location = New System.Drawing.Point(173, 56)
+        Me.lblTime4.Name = "lblTime4"
+        Me.lblTime4.Size = New System.Drawing.Size(24, 16)
+        Me.lblTime4.TabIndex = 5
+        Me.lblTime4.Text = "秒"
+        '
+        'txtTime1
+        '
+        Me.txtTime1.Location = New System.Drawing.Point(102, 53)
+        Me.txtTime1.Name = "txtTime1"
+        Me.txtTime1.Size = New System.Drawing.Size(66, 23)
+        Me.txtTime1.TabIndex = 4
+        Me.txtTime1.Text = "10"
+        Me.txtTime1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblTime2
+        '
+        Me.lblTime2.AutoSize = True
+        Me.lblTime2.Location = New System.Drawing.Point(25, 85)
+        Me.lblTime2.Name = "lblTime2"
+        Me.lblTime2.Size = New System.Drawing.Size(72, 16)
+        Me.lblTime2.TabIndex = 6
+        Me.lblTime2.Text = "変更秒数"
+        '
+        'lblTime5
+        '
+        Me.lblTime5.AutoSize = True
+        Me.lblTime5.Location = New System.Drawing.Point(173, 85)
+        Me.lblTime5.Name = "lblTime5"
+        Me.lblTime5.Size = New System.Drawing.Size(24, 16)
+        Me.lblTime5.TabIndex = 8
+        Me.lblTime5.Text = "秒"
+        '
+        'txtTime2
+        '
+        Me.txtTime2.Location = New System.Drawing.Point(102, 82)
+        Me.txtTime2.Name = "txtTime2"
+        Me.txtTime2.Size = New System.Drawing.Size(66, 23)
+        Me.txtTime2.TabIndex = 7
+        Me.txtTime2.Text = "10"
+        Me.txtTime2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnColor0
+        '
+        Me.btnColor0.Location = New System.Drawing.Point(20, 22)
+        Me.btnColor0.Name = "btnColor0"
+        Me.btnColor0.Size = New System.Drawing.Size(72, 30)
+        Me.btnColor0.TabIndex = 0
+        Me.btnColor0.Text = "背景色"
+        Me.btnColor0.UseVisualStyleBackColor = True
+        '
+        'lblColor0
+        '
+        Me.lblColor0.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblColor0.Location = New System.Drawing.Point(98, 22)
+        Me.lblColor0.Name = "lblColor0"
+        Me.lblColor0.Size = New System.Drawing.Size(94, 30)
+        Me.lblColor0.TabIndex = 1
+        '
+        'lblColor1
+        '
+        Me.lblColor1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblColor1.Location = New System.Drawing.Point(98, 58)
+        Me.lblColor1.Name = "lblColor1"
+        Me.lblColor1.Size = New System.Drawing.Size(94, 30)
+        Me.lblColor1.TabIndex = 3
+        '
+        'btnColor1
+        '
+        Me.btnColor1.Location = New System.Drawing.Point(20, 58)
+        Me.btnColor1.Name = "btnColor1"
+        Me.btnColor1.Size = New System.Drawing.Size(72, 30)
+        Me.btnColor1.TabIndex = 2
+        Me.btnColor1.Text = "事前色"
+        Me.btnColor1.UseVisualStyleBackColor = True
+        '
+        'lblColor2
+        '
+        Me.lblColor2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblColor2.Location = New System.Drawing.Point(98, 94)
+        Me.lblColor2.Name = "lblColor2"
+        Me.lblColor2.Size = New System.Drawing.Size(94, 30)
+        Me.lblColor2.TabIndex = 5
+        '
+        'btnColor2
+        '
+        Me.btnColor2.Location = New System.Drawing.Point(20, 94)
+        Me.btnColor2.Name = "btnColor2"
+        Me.btnColor2.Size = New System.Drawing.Size(72, 30)
+        Me.btnColor2.TabIndex = 4
+        Me.btnColor2.Text = "通常色"
+        Me.btnColor2.UseVisualStyleBackColor = True
+        '
+        'lblColor3
+        '
+        Me.lblColor3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblColor3.Location = New System.Drawing.Point(98, 130)
+        Me.lblColor3.Name = "lblColor3"
+        Me.lblColor3.Size = New System.Drawing.Size(94, 30)
+        Me.lblColor3.TabIndex = 7
+        '
+        'btnColor3
+        '
+        Me.btnColor3.Location = New System.Drawing.Point(20, 130)
+        Me.btnColor3.Name = "btnColor3"
+        Me.btnColor3.Size = New System.Drawing.Size(72, 30)
+        Me.btnColor3.TabIndex = 6
+        Me.btnColor3.Text = "変更色"
+        Me.btnColor3.UseVisualStyleBackColor = True
+        '
+        'frmSetting
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(238, 383)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.gbColor)
+        Me.Controls.Add(Me.gbTime)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmSetting"
+        Me.Text = "動作設定"
+        Me.gbTime.ResumeLayout(False)
+        Me.gbTime.PerformLayout()
+        Me.gbColor.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gbTime As GroupBox
+    Friend WithEvents gbColor As GroupBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents lblTime2 As Label
+    Friend WithEvents lblTime5 As Label
+    Friend WithEvents txtTime2 As TextBox
+    Friend WithEvents lblTime1 As Label
+    Friend WithEvents lblTime4 As Label
+    Friend WithEvents txtTime1 As TextBox
+    Friend WithEvents lblTime0 As Label
+    Friend WithEvents lblTime3 As Label
+    Friend WithEvents txtTime0 As TextBox
+    Friend WithEvents lblColor3 As Label
+    Friend WithEvents btnColor3 As Button
+    Friend WithEvents lblColor2 As Label
+    Friend WithEvents btnColor2 As Button
+    Friend WithEvents lblColor1 As Label
+    Friend WithEvents btnColor1 As Button
+    Friend WithEvents lblColor0 As Label
+    Friend WithEvents btnColor0 As Button
+End Class
